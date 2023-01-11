2280386ac2729324b4c20f0c1009e33321ce687e
cwj <595842570@qq.com>
2023-01-11 19:48:49 +0800

    feat: andriod打包设置

--- lib/constants/constant.dart
+++ lib/constants/constant.dart
@@ -1,15 +1,14 @@
-
 /// @description :常量集
-class Constant{
-
-
+class Constant {
   ///掘金地址
-  static const  juejinLike = "https://juejin.cn/user/553809592465277/posts";
+  static const juejinLike = "https://juejin.cn/user/553809592465277/posts";
+
   ///Github地址
-  static const  githubLike = "https://github.com/cwjbjy/flutter_blog";
+  static const githubLike = "https://github.com/cwjbjy/flutter_blog";
+
   ///WanAndroid地址
-  static const  wanAndroidLike = "https://wanandroid.com/blog/show/2";
+  static const wanAndroidLike = "https://wanandroid.com/blog/show/2";
 
   ///APP下载地址
-  static const downloadUrl = "https://www.pgyer.com/blog";
+  static const downloadUrl = "https://flutter.caowj.top/";
 }
